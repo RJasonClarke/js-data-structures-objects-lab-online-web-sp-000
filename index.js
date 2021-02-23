8c4d51f04b812f3669272618af1024567f2236af
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-23 16:40:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(driver, key, value) {
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   return driver;
 }
 
-function deleteFromObjectByKey(driver, key) {
+function deleteFromDriverByKey(driver, key) {
   var newObject = Object.assign({}, driver);
   delete newObject[key];
   return newObject;
 }
 
 
-function destructivelyDeleteFromObjectByKey(driver, key) {
+function destructivelyDeleteFromDriverByKey(driver, key) {
   delete driver[key];
   return object;
 }
